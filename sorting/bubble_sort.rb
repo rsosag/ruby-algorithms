8370035d5b6aee198df1bfbb9d6836e2f7157c40
Rosel Sosa Gonzalez <rsosa@elasbit.com>
2019-11-27 09:45:02 -0500

    feat: Add bubble_sort.

--- sorting/bubble_sort.rb
+++ sorting/bubble_sort.rb
@@ -1,69 +1,52 @@
 #=======================================================================
 #  Author: Rosel Sosa Gonzalez
-#  Title: Radix Sort
-#  Based on: http://www.geekviewpoint.com/python/sorting/radixsort
-#  Package: algorithms
-#  Category: sorting
+#  Title: Bublle Sort
+#  Based on: http://www.geekviewpoint.com/java/sorting/bubblesort
+#  Package: Algorithms
+#  Category: Sorting
 #
 #  Statement:
-#  Given a disordered list of integers, rearrange them in natural order.
+#  Given a disordered list of integers (or any other items),
+#  rearrange the integers in natural order.
 #
-#  Sample Input: [18,5,100,3,1,19,6,0,7,4,2]
+#  Sample Input: [8,5,3,1,9,6,0,7,4,2,5]
+#  Sample Output: [0,1,2,3,4,5,5,6,7,8,9]
 #
-#  Sample Output: [0,1,2,3,4,5,6,7,18,19,100]
-#
-#  Time Complexity of Solution:
-#  Best Case O(kn); Average Case O(kn); Worst Case O(kn),
-#  where k is the length of the longest number and n is the
-#  size of the input array.
-#
-#  Note: if k is greater than log(n) then an nlog(n) algorithm would
-#  be a better fit. In reality we can always change the radix
-#  to make k less than log(n).
+# Time Complexity of Solution:
+#  Best O(n^2); Average O(n^2); Worst O(n^2).
 #
 #  Approach:
-#  radix sort, like counting sort and bucket sort, is an integer based
-#  algorithm (i.e. the values of the input array are assumed to be
-#  integers). Hence radix sort is among the fastest sorting algorithms
-#  around, in theory. The particular distinction for radix sort is
-#  that it creates a bucket for each cipher (i.e. digit); as such,
-#  similar to bucket sort, each bucket in radix sort must be a
-#  growable list that may admit different keys.
+#   Bubblesort is an elementary sorting algorithm. The idea is to
+#   imagine bubbling the smallest elements of a (vertical) array to the
+#   top; then bubble the next smallest; then so on until the entire
+#   array is sorted. Bubble sort is worse than both insertion sort and
+#   selection sort. It moves elements as many times as insertion sort
+#   (bad) and it takes as long as selection sort (bad). On the positive
+#   side, bubble sort is easy to understand. Also there are highly
+#   improved variants of bubble sort.
 #
-#  For decimal values, the number of buckets is 10, as the decimal
-#  system has 10 numerals/cyphers (i.e. 0,1,2,3,4,5,6,7,8,9). Then
-#  the keys are continuously sorted by significant digits.
+#  0] For each element at index i from 0 to n, loop:
+#  1] For each element at index k, from n to i exclusive, loop:
+#  2] If the element at k is less than that at k-1, swap them.
 #=======================================================================
-class RadixSort
-  BASE = 10
+class BubbleSort
   def self.call(integers_array)
-    max_length = false
-    placement = 1
+    # If size of array is 0 or 1 then it is ordered
+    return integers_array if integers_array.size <= 1
 
-    until max_length do
-      max_length = true
-      # declare and initialize buckets
-      buckets = Array.new(BASE){[]}
-      # split integers_array between arrays
-      integers_array.each do |number|
-        unit = number / placement
-        buckets[unit % BASE].push(number)
-        max_length = false if max_length && unit > 0
-      end
-      # empty array into integers_array array
-      position = 0
-      0.upto(9) do |index|
-        buck = buckets[index]
-        buck.each_with_index do |item|
-          integers_array[position] = item
-          position += 1
+    loop do
+      swapped = false
+      0.upto(integers_array.size-2) do |i|
+        if integers_array[i] > integers_array[i+1]
+          # compare consecutive numbers and swap if it is necessary
+          integers_array[i], integers_array[i+1] = integers_array[i+1], integers_array[i]
+          swapped = true
         end
       end
-      placement *= BASE
+      break unless swapped
     end
+   integers_array
   end
 end
-
 # How to use
-# Add `return integers_array to `#call`
-# Then `execute` RadixSort.call([18, 5, 100, 3, 1, 19, 6, 0, 7, 4, 2])
+# Then `execute` BubbleSort.call([8, 5, 3, 1, 9, 6, 0, 7, 4, 2 ,5])
